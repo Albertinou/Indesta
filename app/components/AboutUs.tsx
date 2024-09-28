df381ce60ed6c90e9ffd1a1ca5939b8118fb7642
Albertinou Albertinou <albertuciukas@gmail.com>
2024-09-28 20:49:36 +0300

    text added to about us

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -44,7 +44,10 @@ export default function AboutUs() {
         </h2>
         <div className="mt-6 text-xl text-gray-500 space-y-6">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Maecenas sed ipsum ut felis congue commodo. Sed quis mauris vitae tortor mollis volutpat. Morbi in dui quis est pulvinar ullamcorper.
+             &nbsp; &nbsp; &nbsp; Esame profesionalų komanda, specializuojanti karkasinių namų ir medinių konstrukcijų statyboje, renovacijoje bei apdailoje. Mūsų tikslas – sukurti funkcionalius ir estetiškus namus, kurie atitiktų jūsų lūkesčius ir poreikius.
+            Su didele patirtimi šioje srityje, mes puikiai išmanome medinių konstrukcijų ypatybes ir galimybes. Kiekvienas projektas mums yra svarbus, todėl stengiamės užtikrinti aukščiausią kokybę, naudojant tik geriausias medžiagas ir modernias technologijas.
+            Mūsų komanda ne tik statybos specialistai, bet ir jūsų partneriai, pasirengę išklausyti, patarti ir įgyvendinti net drąsiausias idėjas. Džiaugiamės galėdami pasiūlyti individualius sprendimus, atitinkančius kiekvieno kliento unikalius poreikius.
+            Pasitikėkite mumis ir leiskite sukurti jūsų svajonių namus, kurie tarnaus ne tik jums, bet ir ateities kartoms. Kartu mes galime pasiekti nuostabių rezultatų!
           </p>
         </div>
       </motion.div>
